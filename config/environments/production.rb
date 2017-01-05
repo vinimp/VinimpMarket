e4ff54d71e128f53be5ccf522c2953fa8e5959c0
vinimp <vinimp@libero.it>
2017-01-05 19:28:00 +0100

    regione 6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   :storage => :s3,
-  :region => ENV['AWS_REGION'],
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
   :bucket => ENV['AWS_BUCKET'],
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
